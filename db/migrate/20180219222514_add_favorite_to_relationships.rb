69932064a0a84ad980e8fff1ae6b329ab1da4ad6
EC2 Default User <ec2-user@ip-172-31-41-214.ec2.internal>
2018-02-25 11:46:02 +0000

    favorite for heroku

--- db/migrate/20180219222514_add_favorite_to_relationships.rb
+++
@@ -1,7 +0,0 @@
-class AddFavoriteToRelationships < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :relationships, :favorite, foreign_key: { to_table: :microposts }
-    
-    t.index [:favorite_id], unique: true
-  end
-end
